9667f024ed1ed68116c5e69eb906454d6bf9c8be
isaitkulov <isaitkulov@ozon.ru>
2024-11-10 18:31:22 +0000

    242 valid anagram, version 2

--- arrays_and_hashing/242_valid_anagram.go
+++ arrays_and_hashing/242_valid_anagram.go
@@ -3,15 +3,23 @@ package arrays_and_hashing
 // временная сложность O(n) - нужно пройтись один раз по обоим строкам
 // память O(n) - нужно хранить мапу под одно слово
 func isAnagram(s string, t string) bool {
-	charsMap := make(map[rune]struct{})
+	charsMap := make(map[rune]int)
 	for _, char := range s {
-		charsMap[char] = struct{}{}
+		charsMap[char]++
 	}
 	for _, char := range t {
 		if _, exists := charsMap[char]; !exists {
 			return false
 		}
+		charsMap[char]--
 	}
 
-	return len(s) == len(t)
+	for _, charsCount := range charsMap {
+		if charsCount != 0 {
+			return false
+		}
+
+	}
+
+	return true
 }
